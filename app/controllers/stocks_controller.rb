86f8733161023947d11972131defd93b9f68907a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-25 00:19:55 -0400

    Handle error exception when invalid stock symbol is entered

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,12 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      render 'users/my_portfolio'
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "Please enter a valid symbol"
+        redirect_to my_portfolio_path
+      end
     else
       flash[:alert] = "Please enter a stock symbol"
       redirect_to my_portfolio_path
