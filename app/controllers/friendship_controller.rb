d68d6e6e29f8217865c97bfdbfa2f1d9e469c357
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-25 20:15:15 -0400

    Handle search form submission

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -2,4 +2,25 @@ class FriendshipController < ApplicationController
   def index
     @friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'friendship/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'friendship/result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'friendship/result' }
+      end
+    end
+  end
 end
