9b527eb5b83caf50455c204fe3f2df9f86136519
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-25 20:25:18 -0400

    Add functionality to search by first name last name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,35 @@ class User < ApplicationRecord
     return false unless stock
     stocks.where(id: stock.id).exists?
   end
+
   def under_stock_limit? 
     stocks.count < 10
   end
+
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name',param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name',param)
+  end
+
+  def self.email_matches(param)
+    matches('email',param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
   end
 end
